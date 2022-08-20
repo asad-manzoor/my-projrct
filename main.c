3860776026b8a4bfc267562ff2696eb05b62cd6b
ASAD-MANZOOR <110839208+ASAD-MANZOOR@users.noreply.github.com>
2022-08-20 10:43:05 +0500

    Update main.c

--- main.c
+++ main.c
@@ -75,7 +75,7 @@ void Add_Student(){
         printf("Enter Address: \n");
         scanf ("%s",&info[i].Address);
        // fgets(info.Address, sizeof(info.Address), stdin);
-        printf("Enter percentage:");
+        printf("Enter per:");
         scanf("%i",&info[i].percentage);
         printf("-------------------------------\n");
         if (fptr == NULL)
